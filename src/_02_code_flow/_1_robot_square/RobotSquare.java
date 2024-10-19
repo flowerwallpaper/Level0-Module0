33ed26b8751c42805360256c56da7ede79dcb668
flowerwallpaper <103080777+flowerwallpaper@users.noreply.github.com>
2024-10-19 00:33:10 +0000

    robot square

--- src/_02_code_flow/_1_robot_square/RobotSquare.java
+++ src/_02_code_flow/_1_robot_square/RobotSquare.java
@@ -19,8 +19,12 @@ public class RobotSquare {
 		// PEN. Put the robot's pen down so it can draw, Use this command:
 		/** rob.penDown(); **/
 
+		rob.penDown();
+
 		// SPEED. Make the robot move quickly. Use this command:
 		/** rob.setSpeed(<speed>); **/
+
+		rob.setSpeed(10);
 
 		// COUNT. Create an int variable that will count how many sides of the square we
 		// have drawn.
@@ -28,15 +32,23 @@ public class RobotSquare {
 		// code:
 		/** int count = 0; **/
 
+		// int count = 0;
+
 		// DRAW. Draw a line. Use this command:
 		/** rob.move(<distance>); **/
+
+		// rob.move(50);
 
 		// TURN. Turn 90 degrees. Use this command:
 		/** rob.turn(<angle>); **/
 
+		// rob.turn(90);
+
 		// INCREASE COUNT. Add one to the number of sides that have now been drawn like
 		// this:
 		/** count += 1; **/
+
+		// count = count + 1; 
 
 		// Run the program. Did the Robot draw a line and turn?
 
@@ -51,6 +63,23 @@ public class RobotSquare {
 		 * INCREASE COUNT
 		 * }
 		 **/
+
+
+		int count = 0;
+		//rob.move(<distance>);
+		//rob.turn(<degree>)
+
+		while(count < 4){
+
+			//move rob forward with rob.move()
+
+			rob.move(200);
+			//turn rob 90 degrees with rob.turn()
+			rob.turn(90);
+
+			count = count + 1; //keeps the loop going
+
+		}
 
 		// Run the program again. Did your Robot draw a square?
 
